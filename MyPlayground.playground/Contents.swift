ae12b04803b04623a579713072ec29b676a1a1d5
«VIKTOR <mrgyugo@yandex.ru>
2017-10-06 12:10:22 +0300

    changes playground for learning

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,9 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-
-
-
-let x:UInt8 = 16
+let session = URLSession(configuration: .default)
+if let url = URL(string: "https://www-media.stanford.edu/wp-content/uploads/2017/03/24182714/about_landing-1.jpg") {
+    
+    
+    let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+        
+        DispatchQueue.main.async {
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+    })
+    
+    task.resume()
+    
+    
+}
