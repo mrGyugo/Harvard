efe69ff8373647e74061add2d24ca69cf9c2037b
«VIKTOR <mrgyugo@yandex.ru>
2017-10-10 17:39:26 +0300

    finish smashtang

--- Smashtag/Smashtag/SmashTweetTableViewController.swift
+++ Smashtag/Smashtag/SmashTweetTableViewController.swift
@@ -16,25 +16,49 @@ class SmashTweetTableViewController: TweetTableViewController {
     var container: NSPersistentContainer? =
         (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
     
-
+    
     override func insertTweets(_ newTweets: [Twitter.Tweet]) {
         super.insertTweets(newTweets)
         updateDatabase(with: newTweets)
     }
-
+    
     private func updateDatabase(with tweets: [Twitter.Tweet]) {
-        container?.performBackgroundTask{ (context) in
+        print("starting database load")
+        container?.performBackgroundTask{ [weak self] context in
             for twitterInfo in tweets {
                 _ = try? Tweet.findOrCreateTweet(matching: twitterInfo, in: context)
             }
+            try? context.save()
+            print("done loading database")
+            self?.printDatabaseStatistics()
         }
-        printDatabaseStatistics()
+        
     }
     
     private func printDatabaseStatistics() {
         if let context = container?.viewContext {
-            
+            context.perform {
+                if let tweetCount = (try? context.fetch(Tweet.fetchRequest()) as [Tweet])?.count {
+                    print("\(tweetCount) tweets")
+                }
+                if let tweeterCount = try? context.count(for: TwitterUser.fetchRequest()) {
+                    print("\(tweeterCount) Twitter users")
+                }
+                
+            }
         }
     }
-
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "Tweeters Mentioning Search Term" {
+            if let tweetersTVS = segue.destination as? SmashTweetersTableViewController {
+                tweetersTVS.mention = searchText
+                tweetersTVS.container = container
+            }
+        }
+        
+        
+    }
 }
